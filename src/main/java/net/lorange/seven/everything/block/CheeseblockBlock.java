ef0b99bd50b5ed6bffdbf92f529b5e0cd1f52e72
Lorange7 <anthonybrewood@172.16.31.236>
2021-04-20 16:06:15 +0100

    quicly adding stuff before my pc is compromized

---
+++ src/main/java/net/lorange/seven/everything/block/CheeseblockBlock.java
@@ -0,0 +1,48 @@
+
+package net.lorange.seven.everything.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.lorange.seven.everything.itemgroup.PetrolItemGroup;
+import net.lorange.seven.everything.EverythingModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@EverythingModElements.ModElement.Tag
+public class CheeseblockBlock extends EverythingModElements.ModElement {
+	@ObjectHolder("everything:cheeseblock")
+	public static final Block block = null;
+	public CheeseblockBlock(EverythingModElements instance) {
+		super(instance, 18);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(PetrolItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("cheeseblock");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
